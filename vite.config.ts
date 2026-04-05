9dc5bbd14d65f58e1524acbcf8f8a515da5a7078
whussien <whussien86@gmail.com>
2026-04-05 18:48:18 +0200

    update base path to /CAPM_V4/ for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CAPM_V3/",
+  base: "/CAPM_V4/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
